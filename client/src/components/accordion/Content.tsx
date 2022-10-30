886201f9c11c4ab563d5096d5da6cf9e374bbf08
Vakon <jamespagedev@outlook.com>
2022-10-30 17:26:27 -0500

    chore: debugging typescript dom scrollHeight

--- client/src/components/accordion/Content.tsx
+++ client/src/components/accordion/Content.tsx
@@ -8,12 +8,13 @@ interface Props {
 function Content({isCollapsed, children}: Props): JSX.Element {
   // variables
   const contentEl = useRef(null);
-  const componentHeight = useRef(200);
+  // const componentHeight = useRef(200);
+  const componentHeight = useRef(0);
   console.log('componentHeight:', componentHeight.current);
 
   // setup
   useLayoutEffect(() => {
-    // componentHeight.current = contentEl.current ? contentEl.current.scrollHeight : 0;
+    componentHeight.current = contentEl.current ? contentEl.current.scrollHeight : 0;
   }, []);
 
   // render
